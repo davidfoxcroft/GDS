8143fce886c273722a2b26b10996aac33723d597
davidfoxcroft <david.foxcroft@gmail.com>
2022-02-13 21:18:45 +0000

    Untrack files in .gitignore

--- oldcode/zetup.R
+++
@@ -1,6 +0,0 @@
-remove.packages("rstan")
-if (file.exists(".RData")) file.remove(".RData")
-
-Sys.setenv(MAKEFLAGS = paste0("-j",parallel::detectCores()))
-install.packages(c("StanHeaders","rstan"),type="source")
-example(stan_model, package = "rstan", run.dontrun = TRUE)
